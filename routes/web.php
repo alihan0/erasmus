78f353214305059b12aacc38c647d4ca0c5ba3db
Alihan Öztürk <alihanozturk364@gmail.com>
2023-11-18 12:20:01 +0300

    user controller created

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MainController;
 use App\Http\Controllers\StaffController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -28,6 +29,18 @@ Route::controller(StaffController::class)->middleware('auth')->prefix('staff')->
     Route::post('/update', 'update');
     Route::post('/change-password', 'change_password');
     Route::post('/remove', 'remove');
+});
+
+Route::controller(UserController::class)->middleware('auth')->prefix('user')->group(function(){
+    Route::get('/', 'all');
+    Route::get('/new', 'new');
+    Route::get('/edit/{id}', 'edit');
+    Route::get('/detail/{id}', 'detail');
+    Route::post('/create', 'create');
+    Route::post('/update', 'update');
+    Route::post('/remove', 'remove');
+    Route::post('/change-password', 'change_password');
+
 });
 
 Route::controller(AuthController::class)->prefix('auth')->group(function(){
